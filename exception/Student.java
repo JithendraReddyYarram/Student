a9980ce9ac878ea5d54535d3edbeb33711e96bd8
JithendraReddy <102645752+JithendraReddyYarram@users.noreply.github.com>
2024-12-25 17:00:09 +0530

    Update Student.java
    Here i have add the field variable mail

--- exception/Student.java
+++ exception/Student.java
@@ -5,17 +5,19 @@ public class Student {
 	private String Name;
 	private String Address;
 	private double Percentage;
+	private String mail;
 	private static int idgen=101;
 	public Student () {
 		Id=idgen;
 		idgen++;
 	}
-	public Student( String name, String address, double percentage) {
+	public Student( String name, String address, double percentage,String mail) {
 		this();
 		
 		Name = name;
 		Address = address;
 		Percentage = percentage;
+		this.mail=mail;
 	}
 	public int getId() {
 		return Id;
@@ -41,6 +43,10 @@ public class Student {
 	public void setPercentage(double percentage) {
 		Percentage = percentage;
 	}
+	public String getMail(){
+		return mail;}
+	public void setMail(String mail){
+		this.mail=mail;}
 	@Override
 	public String toString() {
 		return "Student Id=" + Id + ", Name=" + Name + " ,Address=" + Address + ", Percentage=" + Percentage;
